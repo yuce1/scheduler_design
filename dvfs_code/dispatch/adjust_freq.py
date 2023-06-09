5c934ce1eddb8b26324d08ad9a7acb9b31029a4e
yuce1 <724739722@qq.com>
2023-06-09 15:36:29 +0800

    dvfs调度接口

--- dvfs_code/dispatch/adjust_freq.py
+++ dvfs_code/dispatch/adjust_freq.py
@@ -1,43 +1,121 @@
-import subprocess
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
+import sys
+import re
+import psutil
+import time
+from conf import *
 
-def get_node_labels(node_name):
-    # 执行 kubectl get node 命令并将结果解析为 JSON
-    command = "kubectl get node -o json"
-    result = subprocess.check_output(command, shell=True, text=True)
-    data = json.loads(result)
-
-    # 查找指定节点的标签信息
-    for item in data["items"]:
-        if item["metadata"]["name"] == node_name:
-            return item["metadata"]["labels"]
-
-    return None
+server_power = 0
+server_usage = -1
 
 
+# 解析IPMI，获取整个服务器的实时功耗
+def read_server_power():
+	global server_power
+	# 命令行中要运行的语句
+	cmd1 = "ipmitool sdr elist | grep \"Power\""
+	# 执行以上命令，并且返回结果
+	textlist = os.popen(cmd1).readlines()
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	for text in textlist:
+		a = re.match(r'^Power{2,\s}',text)
+		if a:
+			print(text)
+	# 进行字符串处理
+	temp_list = textlist[0].split("|")
+	server_power = int(temp_list[-1].strip().split(" ")[0])
+
+# 获取整台服务器的利用率
+def read_server_usage():
+	global server_usage
+	server_usage = psutil.cpu_percent(interval=None)
+
+# 获取整台服务器的利用率,执行时间较慢，弃用
+def read_server_usage_old():
+	global server_usage
+	# 命令行中要运行的语句
+	cmd1 = "sar -P ALL -u 1 5"
+	# 执行以上命令，并且返回结果
+	textlist = os.popen(cmd1).readlines()
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	for text in textlist:
+		a = re.match(r'^Average:\s+all\s+\S+\s+',text)
+		if a:
+			temp_list = re.split(r'\s+', text)
+			server_usage = temp_list[2]
+	if server_usage == -1:
+		sys.stderr.write("[ERROR] 无法获取整台服务器的CPU利用率!\n")
+		exit(-1)
+
+# 降低机器的cpu频率
+def down_cpufreq():
+	# 命令行中要运行的语句
+	down_cpufreq_cmd = "echo \"powersave\" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
+	# 执行以上命令，并且返回结果
+	textlist = os.popen(down_cpufreq_cmd).readlines()
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	for text in textlist:
+		print("降低机器频率结果为：" + text)
+
+# 提高机器的cpu频率
+def up_cpufreq():
+	# 命令行中要运行的语句
+	up_cpufreq_cmd = "echo \"performance\" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
+	# 执行以上命令，并且返回结果
+	textlist = os.popen(up_cpufreq_cmd).readlines()
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	for text in textlist:
+		print("升高机器频率结果为：" + text)
+
+# 获取cpu目前所处的状态
+def get_cpu_state():
+	# 命令行中要运行的语句
+	cpu_state_cmd = "cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+	# 执行以上命令，并且返回结果
+	cpu_state = os.popen(cpu_state_cmd).readlines()[0]
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	return cpu_state
 
 
 if __name__ == "__main__":
-	get_node_cmd = "kubectl get nodes"
-	textlist = os.popen(get_node_cmd).readlines()
-	# 异常处理,读取到的文件应该总是一行，进行基本的判断
-	num_node = len(textlist)-1
+	print("执行前查看信息")
+	print(server_power)
+	print(server_usage)
 	i = 1
-	while(i<=num_node):
-		inf_list = textlist[i].strip().split()
-		node_name = inf_list[0]
-		status = inf_list[1]
-		print(node_name)
-		print(status)
-		if(status == "Ready"):
-			# 获取节点的标签信息
-			labels = get_node_labels(node_name)
+	j = 2
+	while True:
+		# read_server_power()
+		# read_server_usage()
+		# print("执行后查看信息")
+		# print(server_power)
+		# print(server_usage)
+		i+=1
+		j+=1
+		server_power = i
+		server_usage = j
+		cpu_state = get_cpu_state()
+		if(server_usage < 20 and cpu_state != "powersave"):
+			print("cpu利用率低，进行频率调节，将频率调小")
+			down_cpufreq()
 
-			if labels:
-				print(f"Node Labels for {node_name}:")
-				for key, value in labels.items():
-					print(f"  {key}: {value}")
-			else:
-				print(f"Node '{node_name}' not found.")
-		i += 1
+		if(server_usage > 20 and cpu_state != "performance"):
+			print("cpu利用率升高，进行频率调节，将频率调大")
+			up_cpufreq()
+		cmd_power = "kubectl label nodes "+ node_name + " power=" + str(server_power) + " --overwrite"
+		# 执行以上命令，并且返回结果
+		textlist = os.popen(cmd_power).readlines()
+		# 异常处理,读取到的文件应该总是一行，进行基本的判断
+		for text in textlist:
+			print(text)
+
+		cmd_usage = "kubectl label nodes "+ node_name + " cpuUse=" + str(server_usage) + " --overwrite"
+		# 执行以上命令，并且返回结果
+		textlist = os.popen(cmd_usage).readlines()
+		# 异常处理,读取到的文件应该总是一行，进行基本的判断
+		for text in textlist:
+			print(text)
+
+
+		time.sleep(0.5)
