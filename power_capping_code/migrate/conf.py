7194ff5b9758605064a07adadb7439125c3b7cc2
yuce1 <724739722@qq.com>
2023-06-14 23:17:41 +0800

    完成powercapping迁移

---
+++ power_capping_code/migrate/conf.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+node_name = "k8s-node"
+# cpu的最大功率
+max_power = 100
+max_proportion = 0.3
+num = 3
+cpu_num = 1
+capping_proportion = 0.25
+
+def set_node_name(value):
+    # 定义一个全局变量
+    global node_name 
+    node_name = value
+
+def get_node_name():
+    global node_name
+    return node_name
+
+def set_max_power(value):
+    # 定义一个全局变量
+    global max_power 
+    max_power = value
+
+def get_max_power():
+    global max_power
+    return max_power
+
+def set_max_proportion(value):
+    # 定义一个全局变量
+    global max_proportion 
+    max_proportion = value
+
+def get_max_proportion():
+    global max_proportion
+    return max_proportion
+
+
+
+def set_cpu_num(value):
+    # 定义一个全局变量
+    global cpu_num 
+    cpu_num = value
+
+def get_cpu_num():
+    global cpu_num
+    return cpu_num
+
+def set_capping_proportion(value):
+    # 定义一个全局变量
+    global capping_proportion 
+    capping_proportion = value
+
+def get_capping_proportion():
+    global capping_proportion
+    return capping_proportion
